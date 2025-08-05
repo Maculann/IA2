e1de8bb26e40631e61504a17d1815ef09b21cfe4
Maculann <ana.maculan@escola.pr.gov.br>
2025-08-05 10:33:27 -0300

    seferf

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: " Ao chegar em casa, você decide explorar mais sobre essa IA. O que você faz primeiro?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
